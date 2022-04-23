617bfb69fd7676181dc31ea20abdd0c72b795ba4
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2022-04-23 13:23:04 +0200

    feat: configure `eslint`

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const rootNode = document.getElementById("root");
+
+if (!rootNode) {
+  throw new Error('fatal: did not find root node in DOM.');
+}
+
+ReactDOM.createRoot(rootNode).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
