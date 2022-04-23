56705b640999e8edb23132feb96c398ed94458e1
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2022-04-23 14:03:34 +0200

    feat: configure `jest` * refactor `App` example to use css modules

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  moduleNameMapper: {
+    "\\.(css|sass|scss)$": "identity-obj-proxy",
+    "\\.(gif|jpg|png|svg)$": "<rootDir>/test/__mocks__/fileMock.js",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+};
