be87942bd1ed6e9c38eead106b074fb05e496828
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2022-04-23 13:23:04 +0200

    feat: configure `eslint`

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  plugins: [],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  env: {
+    browser: true,
+  },
+};
