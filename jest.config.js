c5bb2a3632f731d932428b6087d1b63c05ee1aad
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2022-04-24 11:00:01 +0200

    feat: inline styleMock proxy

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,20 @@
 module.exports = {
-  testEnvironment: "jest-environment-jsdom",
+  testEnvironment: "jsdom",
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
   moduleNameMapper: {
-    "\\.(css|sass|scss)$": "identity-obj-proxy",
+    "\\.(css|sass|scss)$": "<rootDir>/test/__mocks__/styleMock.js",
     "\\.(gif|jpg|png|svg)$": "<rootDir>/test/__mocks__/fileMock.js",
   },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  // required in order for the styleMock proxy to work.
+  // @see https://github.com/keyz/identity-obj-proxy/issues/9#issuecomment-439625933
+  globals: {
+    'ts-jest': {
+      tsconfig: {
+        esModuleInterop: true
+      }
+    }
+  },
 };
---
+++ test/__mocks__/styleMock.js
@@ -0,0 +1,8 @@
+// @see https://github.com/keyz/identity-obj-proxy
+const identityObject = new Proxy({}, {
+  get(_, key) {
+    return key === '__esModule' ? false : key;
+  }
+});
+
+module.exports = identityObject;
