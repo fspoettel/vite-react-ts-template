79893cc3d5d51e709cdbcad56a0eddf02ffebb29
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2022-11-01 16:02:09 +0100

    feat: reduce sample content

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,7 @@
-import logo from "./logo.svg";
 import css from "./App.module.css";
-import { useCounter } from "./hooks/useCounter";
 
 function App() {
-  const [count, increment] = useCounter();
-
-  return (
-    <div className={css["App"]}>
-      <header className={css["App-header"]}>
-        <img src={logo} className={css["App-logo"]} alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={increment}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className={css["App-link"]}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className={css["App-link"]}
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+  return <main className={css["app"]}>React ❤️ Vite</main>;
 }
 
 export default App;
--- src/hooks/useCounter.ts
+++
@@ -1,8 +0,0 @@
-import { useCallback, useState } from "react";
-
-export function useCounter(): [number, () => void] {
-  const [count, setCount] = useState(0);
-  const increment = useCallback(() => setCount((count) => count + 1), []);
-
-  return [count, increment];
-}
