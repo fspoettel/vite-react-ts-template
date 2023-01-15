0f04bd6fdcf2bb358edb005ac59bdb24ece9399d
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2023-01-15 15:48:04 +0100

    feat: use consistent config locations

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [require("autoprefixer")],
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
