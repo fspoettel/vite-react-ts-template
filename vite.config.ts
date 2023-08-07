a7d4251556976896a36e37418f18eee8d23d017b
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2023-08-07 11:26:20 +0200

    fix: move postcss config to own file

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [require("autoprefixer")],
-    },
-  },
   test: {
     globals: true,
     environment: "jsdom",
