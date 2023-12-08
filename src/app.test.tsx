a05c9fe8c6d2d34c46748575829a27012a6394f5
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2023-12-08 17:23:55 +0100

    feat: use lowercase for all files

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,6 +1,6 @@
 import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import App from "./app";
 
 test("renders the example page", async () => {
   render(<App />);
